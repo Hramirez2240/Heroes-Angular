21162185839754d62fcf92b4b72defd63d7be490
hramirez2240 <hramirez2298@gmail.com>
2022-06-08 23:46:08 -0400

    feature: first commit

---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from 'src/app/services/heroes.service';
+import { Heroe } from 'src/app/services/heroes.service';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styles: [
+  ]
+})
+export class HeroComponent implements OnInit {
+
+  hero: any = {};
+
+  constructor(private _activatedRoute: ActivatedRoute, private _heroesService:HeroesService) {
+    this._activatedRoute.params.subscribe(params => {
+      this.hero = this._heroesService.getHero(params['id']);
+    });
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
